b5c94a57e6799fa832bac09c06b3d5576b7a8351
Pipe <pipe.evc@gmail.com>
2021-10-26 04:12:04 -0300

    agregamos solucion del AP

---
+++ src/main/kotlin/Consultas.kt
@@ -0,0 +1,138 @@
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun main() {
+    Database.connect(
+        "jdbc:mariadb://localhost:3306/cofradia",
+        "org.mariadb.jdbc.Driver",
+        "root",
+        "local"
+    )
+
+    transaction {
+
+        exposedLogger.info("1. Cazadores que en su rango tienen 'en'.")
+        Cazadores.select { Cazadores.rango like "%en%" }.forEach { cazador ->
+            exposedLogger.info(cazador.toString())
+        }
+
+        exposedLogger.info("----------------------------------")
+        exposedLogger.info("")
+        exposedLogger.info("2. Entrentamiento con mayor cantidad de involucrados el 2021")
+        val personasEnfretamiento = mutableMapOf<Int, MutableList<String>>()
+        (DetalleEnfrentamientos innerJoin Enfrentamientos)
+            .slice(
+                DetalleEnfrentamientos.id_cazador,
+                DetalleEnfrentamientos.id_demonio,
+                Enfrentamientos.id_enfrentamiento,
+                Enfrentamientos.fecha
+            )
+            .select { Enfrentamientos.fecha.greaterEq("2021-01-01 00:00:00") }
+            .orderBy(Enfrentamientos.id_enfrentamiento to SortOrder.DESC)
+            .forEach {
+                println(it)
+
+                //agregamos cazadores
+                personasEnfretamiento
+                    .putIfAbsent(
+                        it[Enfrentamientos.id_enfrentamiento],
+                        mutableListOf("Cazador#" + it[DetalleEnfrentamientos.id_cazador])
+                    )?.add("Cazador#" + it[DetalleEnfrentamientos.id_cazador])
+
+                //agregamos demonios
+                personasEnfretamiento
+                    .putIfAbsent(
+                        it[Enfrentamientos.id_enfrentamiento],
+                        mutableListOf("Demonio#" + it[DetalleEnfrentamientos.id_demonio])
+                    )?.add("Demonio#" + it[DetalleEnfrentamientos.id_demonio])
+
+                //limpiamos duplicados
+                personasEnfretamiento[it[Enfrentamientos.id_enfrentamiento]] =
+                    personasEnfretamiento[it[Enfrentamientos.id_enfrentamiento]]?.distinct() as MutableList<String>
+            }
+        exposedLogger.info(personasEnfretamiento.toString())
+
+        //buscamos el mayor
+        val maxPersonasEnfretamiento = personasEnfretamiento.maxByOrNull { t -> t.value.count() }
+        exposedLogger.info(maxPersonasEnfretamiento.toString())
+
+        exposedLogger.info("----------------------------------")
+        exposedLogger.info("")
+        exposedLogger.info("3. Ataques que más se repiten para Cazador con Tan o Jiro")
+        var maxAtaques = 0L
+        val ataques = (Cazadores innerJoin CazadoresAtaques innerJoin Ataques)
+            .slice(
+                Ataques.nombre_ataque,
+                Ataques.id_ataque,
+                Cazadores.nombre,
+                Ataques.id_ataque.count()
+            )
+            .select { (Cazadores.nombre like "%pi%") or (Cazadores.nombre like "%jiro%") }
+            .groupBy(Ataques.id_ataque)
+            .orderBy(Ataques.id_ataque.count() to SortOrder.DESC)
+            .filter {
+                if (it[Ataques.id_ataque.count()] > maxAtaques) {
+                    maxAtaques = it[Ataques.id_ataque.count()]
+                }
+                it[Ataques.id_ataque.count()] == maxAtaques
+            }
+            .map {
+                return@map it
+            }
+        exposedLogger.info(ataques.toString())
+
+        exposedLogger.info("----------------------------------")
+        exposedLogger.info("")
+        exposedLogger.info("4. Demonio más resistente")
+        var maxDemonioMasResistente = 0L
+        val demonioMasResistente = (DetalleEnfrentamientos innerJoin Demonios)
+            .slice(
+                Demonios.nombre,
+                DetalleEnfrentamientos.estado_demonio,
+                Demonios.id_demonio.count()
+            )
+            .selectAll()
+            .groupBy(Demonios.id_demonio, DetalleEnfrentamientos.estado_demonio)
+            .having { DetalleEnfrentamientos.estado_demonio like "vivo" }
+            .orderBy(Demonios.id_demonio.count() to SortOrder.DESC)
+            .map {
+                if (it[Demonios.nombre].contains("uno")) {
+                    it[Demonios.nombre] = "Barby"
+                }
+                return@map it
+            }
+            .filter {
+                if (it[Demonios.id_demonio.count()] > maxDemonioMasResistente) {
+                    maxDemonioMasResistente = it[Demonios.id_demonio.count()]
+                }
+                it[Demonios.id_demonio.count()] == maxDemonioMasResistente
+            }
+        exposedLogger.info(demonioMasResistente.toString())
+
+        exposedLogger.info("----------------------------------")
+        exposedLogger.info("")
+        exposedLogger.info("5. Demonios matados por Tanjiro")
+        val demoniosTanjiro = (Cazadores innerJoin DetalleEnfrentamientos innerJoin Demonios)
+            .slice(
+                Demonios.nombre,
+                DetalleEnfrentamientos.estado_demonio
+            )
+            .select {
+                (DetalleEnfrentamientos.estado_demonio like "muerto") and (Cazadores.nombre like "Pipe%")
+            }
+            .map {
+                return@map it
+            }
+        exposedLogger.info(demoniosTanjiro.toString())
+
+        exposedLogger.info("----------------------------------")
+        exposedLogger.info("")
+        exposedLogger.info("Extra1. Revisamos vista")
+        val vista = Top10
+            .selectAll()
+            .map {
+                return@map it
+            }
+        exposedLogger.info(vista.toString())
+    }
+}
--- src/main/kotlin/Exposed.kt
+++ src/main/kotlin/Exposed.kt
@@ -16,13 +16,52 @@ object Cazadores : Table() {
 object Ataques : Table() {
     val id_ataque = integer("id_ataque")
     val nombre_ataque = varchar("nombre_ataque", 50)
-    val arma =  varchar("arma", 50)
+    val arma = varchar("arma", 50)
     override val primaryKey = PrimaryKey(id_ataque)
 }
 
-object CazadoresAtaques : Table() {
+object CazadoresAtaques : Table("cazadores_ataques") {
     val id_cazador_ataque = integer("id_cazador_ataque")
     val id_cazador = integer("id_cazador") references Cazadores.id_cazador
     val id_ataque = integer("id_ataque") references Ataques.id_ataque
     override val primaryKey = PrimaryKey(id_cazador_ataque)
+}
+
+object Demonios : Table() {
+    val id_demonio = integer("id_demonio")
+    val nombre = varchar("nombre", 50)
+    val rango = varchar("rango", 50)
+    override val primaryKey = PrimaryKey(id_demonio)
+}
+
+object DetalleEnfrentamientos : Table("detalle_enfrentamientos") {
+    val id_detalle_enfrentamiento = integer("id_detalle_enfrentamiento")
+    val id_cazador = integer("id_cazador") references Cazadores.id_cazador
+    val id_demonio = integer("id_demonio") references Demonios.id_demonio
+    val id_enfrentamiento = integer("id_enfrentamiento") references Enfrentamientos.id_enfrentamiento
+    val estado_cazador = varchar("estado_cazador", 50)
+    val estado_demonio = varchar("estado_demonio", 50)
+    override val primaryKey = PrimaryKey(id_detalle_enfrentamiento)
+}
+
+object Enfrentamientos : Table() {
+    val id_enfrentamiento = integer("id_enfrentamiento")
+    val fecha = datetime("fecha").defaultExpression(CurrentDateTime())
+    override val primaryKey = PrimaryKey(id_enfrentamiento)
+}
+
+object LogsName : Table("logs_name") {
+    val id_log = integer("id_log")
+    val id_cazador = integer("id_cazador")
+    val nombre = varchar("nombre", 50)
+    val fecha = datetime("fecha").defaultExpression(CurrentDateTime())
+    override val primaryKey = PrimaryKey(id_log)
+}
+
+object Top10 : Table("top10") {
+    val id_cazador = integer("id_cazador")
+    val nombre = varchar("nombre", 50)
+    val birthday = datetime("birthday")
+    val rango = varchar("rango", 50)
+    val pais_origen = varchar("pais_origen", 50)
 }
